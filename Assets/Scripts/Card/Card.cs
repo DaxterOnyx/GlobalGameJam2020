2f0651d117ade9a7bbe682a638dcba16c61a606b
Daxter Onyx <daxter5onyx@gmail.com>
2020-02-01 00:07:42 +0100

    CardPrefab

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
 	public CardData data;
-	public SpriteRenderer ImageRenderer;
+	public Image Image;
+	public Text Name;
+	public Text Cost;
+	public Text Description;
 	private void Start()
 	{
-		ImageRenderer.sprite = data.Image;
+		Image.sprite = data.Image;
+		Name.text = data.Name;
+		Cost.text = data.Cost.ToString();
+		Description.text = data.Description;
 	}
 
 	private void Update()
