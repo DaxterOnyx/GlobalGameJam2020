4c43acd55701242360c8153823f23e67bb3a9636
Leon <romain.delpy@hotmail.com>
2020-02-01 21:35:04 +0100

    Real Merge

--- Assets/Scripts/ScriptableObjects/CharaData/CharacterData.cs
+++ Assets/Scripts/ScriptableObjects/CharaData/CharacterData.cs
@@ -9,27 +9,4 @@ public abstract class CharacterData : ScriptableObject
     public int nbActionPoint;
 }
 
-<<<<<<< HEAD:Assets/Scripts/ScriptableObjects/CharaData/CharacterData.cs
-=======
-[CreateAssetMenu(fileName = "PlayerData", menuName = "Data/PlayerData")]
-public class PlayerData : CharacterData
-{
-	internal int Strengh;
-	internal int FireGunDamage;
-}
-
-[CreateAssetMenu(fileName = "MonsterData", menuName = "Data/MonsterData")]
-public class MonsterData : CharacterData
-{
-    public target target;
-}
-
-[CreateAssetMenu(fileName = "ObjectData", menuName = "Data/ObjectData")]
-public class ObjectData : CharacterData
-{
-    public int repairCount;
-    public bool isObjective;
-}
-
->>>>>>> master:Assets/Scripts/ScriptableObjects/CharacterData.cs
 public enum target { Player, Objective };
