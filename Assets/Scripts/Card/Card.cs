b8ffb9d415a0edb6fd67379056084504c2638619
Daxter Onyx <daxter5onyx@gmail.com>
2020-02-02 17:57:26 +0100

    New Font

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -20,7 +20,8 @@ public class Card : MonoBehaviour
 	public GameObject Back;
 
 	delegate void Action();
-	Action action;
+	/*List<(float,*/
+	Action/*)>*/ actions;
 	float actionTimer;
 
 	private bool interactable;
@@ -37,6 +38,7 @@ public class Card : MonoBehaviour
 
 	private void Start()
 	{
+		//actions = new List<(float, Action)>();
 		if (data != null)
 			Init(data);
 	}
@@ -54,158 +56,164 @@ public class Card : MonoBehaviour
 
 	private void Update()
 	{
-		if (action != null) {
+		//if (actions.Count > 0) {
+		//for (int i = 0; i < actions.Count; i++) {
+		//actions[i].Item1 = Time.deltaTime;
+		if (actions != null) {
+
 			actionTimer -= Time.deltaTime;
 			if (actionTimer <= 0) {
-				action();
-				action = null;
+				actions();
+				actions = null;
 			}
 		}
+	//}
+	//}
+}
+
+internal void ActionCard(Player actor, Character target)
+{
+	if (data.Cost > actor.actionLeft) {
+		Debug.LogError("Not enough action point");
+		return;
 	}
 
-	internal void ActionCard(Player actor, Character target)
-	{
-		if (data.Cost > actor.actionLeft) {
-			Debug.LogError("Not enough action point");
-			return;
-		}
+	actor.UseActionPoint(data.Cost);
 
-		actor.UseActionPoint(data.Cost);
-
-		switch (data.Action) {
-			case CardData.CardAction.CaC:
-				actor.Kick();
-				target.TakeDamage(actor.data.Strengh);
-				break;
-			case CardData.CardAction.Armor:
-			//TODO ADD ARMOR EFFECT
+	switch (data.Action) {
+		case CardData.CardAction.CaC:
+			actor.Kick();
+			target.TakeDamage(actor.data.Strengh);
+			break;
+		case CardData.CardAction.Armor:
+		//TODO ADD ARMOR EFFECT
+		//TODO ADD VISUAL EFFECT
+		case CardData.CardAction.Heal:
 			//TODO ADD VISUAL EFFECT
-			case CardData.CardAction.Heal:
-				//TODO ADD VISUAL EFFECT
-				//TODO REMOVE HARC VALUE
-				target.TakeDamage(-2);
+			//TODO REMOVE HARC VALUE
+			target.TakeDamage(-2);
+			break;
+		case CardData.CardAction.Attack:
+			actor.Shoot();
+			target.TakeDamage(actor.data.FireGunDamage);
+			break;
+		case CardData.CardAction.Repair:
+			//TODO REMOVE Hard Value
+			(target as Object).Repair(25);
+			break;
+		default:
+			Debug.LogError("Not Defined Action : " + data.Action.ToString());
+			break;
+	}
+
+	CardManager.Instance.DiscardCard(this);
+}
+
+public bool IsValableTarget(Player actor, Character target)
+{
+	bool isValable = false;
+	//Check distance
+	if (!MapManager.Instance.WhereIsObject(target.gameObject, out var targetPos)
+	 || !MapManager.Instance.WhereIsObject(actor.gameObject, out var actorPos))
+		return false;
+
+	if (actorPos != targetPos && Pathfinding.Instance.findPath(actorPos, targetPos).Count > data.Range)
+		return false;
+
+	//Check target match
+	for (int i = 0; i < data.targetTypes.Length && !isValable; i++) {
+		switch (data.targetTypes[i]) {
+			case CardData.TargetType.Player:
+				if (target is Player)
+					isValable = true;
 				break;
-			case CardData.CardAction.Attack:
-				actor.Shoot();
-				target.TakeDamage(actor.data.FireGunDamage);
+			case CardData.TargetType.Himself:
+				if (target == actor)
+					isValable = true;
 				break;
-			case CardData.CardAction.Repair:
-				//TODO REMOVE Hard Value
-				(target as Object).Repair(25);
+			case CardData.TargetType.Monster:
+				if (target is Monster)
+					isValable = true;
+				break;
+			case CardData.TargetType.Object:
+				if (target is Object)
+					isValable = true;
 				break;
 			default:
-				Debug.LogError("Not Defined Action : " + data.Action.ToString());
+				Debug.LogError("Not Defined Type : " + data.targetTypes[i].ToString());
+
 				break;
 		}
-
-		CardManager.Instance.DiscardCard(this);
 	}
 
-	public bool IsValableTarget(Player actor, Character target)
-	{
-		bool isValable = false;
-		//Check distance
-		if (!MapManager.Instance.WhereIsObject(target.gameObject, out var targetPos)
-		 || !MapManager.Instance.WhereIsObject(actor.gameObject, out var actorPos))
-			return false;
+	return isValable;
+}
 
-		if (actorPos != targetPos && Pathfinding.Instance.findPath(actorPos, targetPos).Count > data.Range)
-			return false;
+internal void Unselect()
+{
+	if (GameManager.Instance.CardSelected == this)
+		GameManager.Instance.CardSelected = null;
+	animator.SetBool("Selected", false);
+}
 
-		//Check target match
-		for (int i = 0; i < data.targetTypes.Length && !isValable; i++) {
-			switch (data.targetTypes[i]) {
-				case CardData.TargetType.Player:
-					if (target is Player)
-						isValable = true;
-					break;
-				case CardData.TargetType.Himself:
-					if (target == actor)
-						isValable = true;
-					break;
-				case CardData.TargetType.Monster:
-					if (target is Monster)
-						isValable = true;
-					break;
-				case CardData.TargetType.Object:
-					if (target is Object)
-						isValable = true;
-					break;
-				default:
-					Debug.LogError("Not Defined Type : " + data.targetTypes[i].ToString());
+internal void Select()
+{
+	animator.SetBool("Selected", true);
+}
 
-					break;
-			}
-		}
+internal void Discard(float time)
+{
+	actions = ReelDiscard;
+	actionTimer = time;
+	Disapear(time);
+}
 
-		return isValable;
-	}
+public void Disapear(float time)
+{
+	actions = Disapear;
+	actionTimer = time;
+}
 
-	internal void Unselect()
-	{
-		if (GameManager.Instance.CardSelected == this)
-			GameManager.Instance.CardSelected = null;
-		animator.SetBool("Selected", false);
-	}
+private void Disapear()
+{
+	gameObject.SetActive(false);
+}
 
-	internal void Select()
-	{
-		animator.SetBool("Selected", true);
-	}
+public void Show()
+{
+	gameObject.SetActive(true);
 
-	internal void Discard(float time)
-	{
-		action = ReelDiscard;
-		actionTimer = time;
-		Disapear(time);
-	}
+}
 
-	public void Disapear(float time)
-	{
-		action = Disapear;
-		actionTimer = time;
-	}
+void ReelDiscard()
+{
+	Return(false);
+}
 
-	private void Disapear()
-	{
-		gameObject.SetActive(false);
-	}
+public void Return(bool faced)
+{
+	Face.SetActive(faced);
+	Back.SetActive(!faced);
+}
 
-	public void Show()
-	{
-		gameObject.SetActive(true);
+public void OnPointerClick(PointerEventData eventData)
+{
+	animator.SetTrigger("Click");
+	RecTransform.SetAsLastSibling();
+	if (Interactable)
+		GameManager.Instance.SelectCard(this);
+}
 
-	}
+public void OnPointerEnter(PointerEventData eventData)
+{
+	CardManager.Instance.MouseOver = true;
+	RecTransform.SetAsLastSibling();
+	animator.SetBool("Over", true);
+}
 
-	void ReelDiscard()
-	{
-		Return(false);
-	}
-
-	public void Return(bool faced)
-	{
-		Face.SetActive(faced);
-		Back.SetActive(!faced);
-	}
-
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		animator.SetTrigger("Click");
-		RecTransform.SetAsLastSibling();
-		if (Interactable)
-			GameManager.Instance.SelectCard(this);
-	}
-
-	public void OnPointerEnter(PointerEventData eventData)
-	{
-		CardManager.Instance.MouseOver = true;
-		RecTransform.SetAsLastSibling();
-		animator.SetBool("Over", true);
-	}
-
-	public void OnPointerExit(PointerEventData eventData)
-	{
-		CardManager.Instance.MouseOver = false;
-		animator.SetBool("Over", false);
-	}
+public void OnPointerExit(PointerEventData eventData)
+{
+	CardManager.Instance.MouseOver = false;
+	animator.SetBool("Over", false);
+}
 }
