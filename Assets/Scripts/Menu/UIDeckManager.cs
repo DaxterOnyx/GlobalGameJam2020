152433833ee22367f457a5d3414f05a0d0d11fda
Daxter Onyx <daxter5onyx@gmail.com>
2020-02-09 18:03:00 +0100

    character Selection work in progress

---
+++ Assets/Scripts/Menu/UIDeckManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDeckManager : MonoBehaviour
+{
+	private static UIDeckManager _instance;
+	public static UIDeckManager Instance
+	{
+		get {
+			if (_instance == null)
+				_instance = FindObjectOfType<UIDeckManager>();
+			return _instance;
+		}
+	}
+
+	[SerializeField]
+	public Dictionary<Card,int> deck;
+
+	public 
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		_instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Menu/UIPlayerDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerDisplay : MonoBehaviour
+{
+	public PlayerData data;
+
+	public Image ImageDisplay;
+	public TextMeshProUGUI NameDisplay;
+	public TextMeshProUGUI ActionDisplay;
+	public TextMeshProUGUI LifeDisplay;
+	public TextMeshProUGUI StrenghDisplay;
+	public TextMeshProUGUI GunFireDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (data == null) {
+			Debug.LogError("WHERE IS MY DATA, WHO AM I ?");
+			return;
+		}
+
+		//TODO PLAYER IMAGE DISPLAY
+		NameDisplay.text = data.name;
+		LifeDisplay.text = data.nbMaxLP.ToString();
+		ActionDisplay.text = data.nbActionPoint.ToString();
+		StrenghDisplay.text = data.Strengh.ToString();
+		GunFireDisplay.text = data.FireGunDamage.ToString(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
